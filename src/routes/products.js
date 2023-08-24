8025bcd451c61507e89790de4c92c60eb5b36642
LucasIbanez13 <lucaas00129@gmail.com>
2023-08-24 13:48:51 -0300

    agrego funcion create

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const {shoppingCart,productDetail,productCreate,productEdit} = require('../controllers/controllerProducts');
+const {shoppingCart,productDetail,productCreate,productEdit,productUpdate} = require('../controllers/controllerProducts');
 const router = express.Router();
 
 /* /products */
 router.get('/shoppingCart', shoppingCart);
 router.get('/productDetail/:id/',productDetail)
-router.get('/productCreate/:id/',productCreate)
-router.get('/productEdit/:id/',productEdit)
+router.get('/productCreate',productCreate)
+router.post('/productCreate',productUpdate)
+router.get('/productEdit/:id?',productEdit)
 
 
 
