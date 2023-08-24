b2560feb190a0ab3588b2c1dbd9107baae1ed22d
gabitoo21 <gabi_vega123@outlook.com>
2023-08-24 19:32:43 -0300

    corrigo merge

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,26 +1,15 @@
 const express = require('express');
-<<<<<<< HEAD
 const {shoppingCart,productDetail,productCreate,productEdit,} = require('../controllers/controllerProducts');
 const { remove } = require('../controllers/funciones/remove');
 
-=======
-const {shoppingCart,productDetail,productCreate,productEdit,productUpdate} = require('../controllers/controllerProducts');
->>>>>>> 0284461e5674ecd45ae3de3e398fb0d2d391a3c9
 const router = express.Router();
 
 /* /products */
 router.get('/shoppingCart', shoppingCart);
 router.get('/productDetail/:id/',productDetail)
-<<<<<<< HEAD
 router.get('/productCreate/:id/',productCreate)
 router.get('/productEdit/:id/',productEdit)
 router.delete("/remove/:id/",remove);
-=======
-router.get('/productCreate',productCreate)
-router.post('/productCreate',productUpdate)
-router.get('/productEdit/:id?',productEdit)
-
->>>>>>> 0284461e5674ecd45ae3de3e398fb0d2d391a3c9
 
 
 module.exports = router;
