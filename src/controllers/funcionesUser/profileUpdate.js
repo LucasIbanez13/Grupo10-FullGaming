cb8478c0a4855836fbc34ba66b60c6fea2bdc33a
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-10-23 22:27:33 -0300

    profile

--- src/controllers/funcionesUser/profileUpdate.js
+++ src/controllers/funcionesUser/profileUpdate.js
@@ -1,38 +1,39 @@
-const fs = require("fs");
-const path = require("path");
-
-const usersFilePath = path.join(__dirname, '../../data/user.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-
-
+const db = require('../../database/models')
 
 module.exports = {
     profileUpdate: (req, res) => {
         const profileId = req.params.id;
-        users.forEach(profile => {
-            
-            if (profile.id == profileId) {
-
-                profile.image = req.file ? req.file.filename : 'auricular.png';
-                profile.name = req.body.name,
-                profile.surname = req.body.surname,
-                profile.phone = req.body.phone,
-                profile.dni = req.body.dni,
-                profile.lookfor = req.body.lookfor,
-                profile.street =req.body. street,
-                profile.height =req.body. height,
-                profile.floor =req.body. floor,
-                profile.department =req.body. department,
-                profile.references =req.body. references,
-                profile.postalcode =req.body. postalcode,
-                profile.provinces =req.body. provinces,
-                profile.location =req.body. location
         
-            }
-
-        });
-        fs.writeFileSync(usersFilePath, JSON.stringify(users), 'utf-8');
     
-        res.redirect(`/users/profile/${profileId}`);
-    },
+        const updatedProfile = {
+            image: req.file ? req.file.filename : 'auricular.png',
+            name: req.body.name,
+            surname: req.body.surname,
+            phone : req.body.phone,
+            dni: req.body.dni,
+            lookfor: req.body.lookfor,
+            street: req.body.street,
+            height: req.body.height,
+            floor: req.body.floor,
+            department: req.body.department,
+            references: req.body.references,
+            postalcode: req.body.postalcode,
+            provinces: req.body.provinces,
+            location: req.body.location
+        };
+    
+        const whereCondition = {
+            id: profileId
+        };
+    
+        db.User.update(updatedProfile, {
+            where: whereCondition
+        })
+        .then(() => {
+            res.redirect(`/users/profile/${profileId}`);
+        })
+        .catch(error => {
+            console.log("Error al actualizar el perfil de usuario:", error);
+        });
+    }
 }
