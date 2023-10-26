7d5289904ce2c5d4e6e48a51ed017ce2eff9d97a
gabitoo21 <gabi_vega123@outlook.com>
2023-10-26 15:27:23 -0300

    refactorizo remove

--- src/controllers/funciones/remove.js
+++ src/controllers/funciones/remove.js
@@ -1,25 +1,43 @@
-const db = require("../../database/models")
+const db = require("../../database/models");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-    remove: (req, res) => {
-      const productId = req.params.id;
-  
+  remove: async (req, res) => {
+    const productId = req.params.id;
+
+    try {
       
-      db.Image.destroy({
+      await db.Image.destroy({
         where: { productId: productId }
-      })
-        .then(() => {
-          
-          db.Product.destroy({
-            where: { id: productId }
-          })
-            .then(() => {
-              res.redirect("/users/admin");
-            })
-            .catch((error) =>
-              console.error("error al eliminar producto:", error));
-        })
-        .catch((error) =>
-          console.error("error al eliminar las imagenes:", error));
+      });
+
+      
+      const product = await db.Product.findByPk(productId);
+
+      if (product && product.image) {
+        //aca vamos a construir la ruta de la ubicacion donde se alojan las imagenes
+        const imagePath = path.join(__dirname, "../../../public/img", product.image);
+
+        // Aquí implementamos la lógica para eliminar el archivo en imagePath
+        fs.unlink(imagePath, (error) => {
+          if (error) {
+            console.error("**********************Error al eliminar la imagen principal:", error);
+          } else {
+            console.log("****************************8Imagen principal eliminada correctamente.");
+          }
+        });
+      }
+
+      
+      await db.Product.destroy({
+        where: { id: productId }
+      });
+
+      res.redirect("/users/admin");
+    } catch (error) {
+      console.error("Error al eliminar producto o imágenes:", error);
+      
     }
-  };
+  }
+};
