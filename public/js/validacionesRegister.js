f910961b922fce45fd663aa9fb9f0b5d29a33637
GonzaSotelo <g.sotelo.dab@gmail.com>
2023-11-22 10:04:43 -0300

    m

--- public/js/validacionesRegister.js
+++ public/js/validacionesRegister.js
@@ -1,42 +1,45 @@
-console.log("El archivo validation.js se ha cargado correctamente.");
-document.addEventListener("DOMContentLoaded", function() {
-    const registerForm = document.querySelector("form");
-    const nameInput = document.querySelector("input[name='name']");
-    const emailInput = document.querySelector("input[name='email']");
-    const passInput = document.querySelector("input[name='pass']");
-    const passzInput = document.querySelector("input[name='passz']");
-    const errorMessages = Array.from(document.querySelectorAll(".error-message"));
+window.addEventListener('load', function(){
+    let formulario = document.querySelector(".register"); 
+    formulario.addEventListener("submit", function(e){
+     
 
-    registerForm.addEventListener("submit", function(event) {
-        let isValid = true;
+        let errores = [];
 
+        let formName = document.querySelector('#formName');
         
-        if (nameInput.value.trim() === "") {
-            errorMessages[0].textContent = "Por favor, ingrese un nombre.";
-            isValid = false;
+        if (formName.value == "") {
+            errores.push("El campo de nombre no puede estar vacío");
+        } else if (formName.value.length < 3)
+        errores.push("el nombre tiene que tener al menos  3 caracteres")
+
+        let formEmail = document.querySelector('#formEmail');
+        
+        if (formEmail.value == "") {
+            errores.push("El campo de email no puede estar vacío");
+        }
+
+
+        let formPass = document.querySelector('#formPass');
+        
+        if (formPass.value == "") {
+            errores.push("El campo de Pass no puede estar vacío");
         }
 
         
-        if (emailInput.value.trim() === "") {
-            errorMessages[1].textContent = "Por favor, ingrese un correo electrónico.";
-            isValid = false;
+        let formPass2 = document.querySelector('#formPass2');
+        
+        if (formPass2 == formPass) {
+            errores.push("El campo no puede ser distinto");
         }
 
-        
-        if (passInput.value.trim() === "") {
-            errorMessages[2].textContent = "Por favor, ingrese una contraseña.";
-            isValid = false;
+        if (errores.length > 0) {
+            e.preventDefault();
+        }
+        let ulErrores = document.querySelector("div.errores ul");
+        for (let i = 0; i < errores.length; i++) {
+            ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+            
         }
 
-        
-        if (passzInput.value.trim() === "" || passzInput.value !== passInput.value) {
-            errorMessages[3].textContent = "Las contraseñas no coinciden.";
-            isValid = false;
-        }
-
-        
-        if (!isValid) {
-            event.preventDefault();
-        }
     });
 });
