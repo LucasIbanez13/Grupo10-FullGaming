fbb290de88636f3602ddc1045d1efb9632b6322e
LucasIbanez13 <lucaas00129@gmail.com>
2023-12-13 19:36:47 -0300

    agrego condiciones y estilo en home

---
+++ public/javascripts/condiciones.js
@@ -0,0 +1,60 @@
+function mostrarOcultarParrafo() {
+    var parrafoSeguridad = document.getElementById("parrafoSeguridad");
+
+    // Verifica si el párrafo está visible
+    if (parrafoSeguridad.style.display === "none" || parrafoSeguridad.style.display === "") {
+        // Si está oculto, lo muestra
+        parrafoSeguridad.style.display = "block";
+    } else {
+        // Si está visible, lo oculta
+        parrafoSeguridad.style.display = "none";
+    }
+}
+function mostrarOcultarDivTerminos() {
+    var divTerminos = document.getElementById("divTerminos");
+
+    // Verifica si el div está visible
+    if (divTerminos.style.display === "none" || divTerminos.style.display === "") {
+        // Si está oculto, lo muestra
+        divTerminos.style.display = "block";
+    } else {
+        // Si está visible, lo oculta
+        divTerminos.style.display = "none";
+    }
+}
+function mostrarOcultarDivBoton() {
+    var divBoton = document.getElementById("divBoton");
+
+    // Verifica si el div está visible
+    if (divBoton.style.display === "none" || divBoton.style.display === "") {
+        // Si está oculto, lo muestra
+        divBoton.style.display = "block";
+    } else {
+        // Si está visible, lo oculta
+        divBoton.style.display = "none";
+    }
+}
+function mostrarOcultarDivGarantia() {
+    var divGarantia = document.getElementById("divGarantia");
+
+    // Verifica si el div está visible
+    if (divGarantia.style.display === "none" || divGarantia.style.display === "") {
+        // Si está oculto, lo muestra
+        divGarantia.style.display = "block";
+    } else {
+        // Si está visible, lo oculta
+        divGarantia.style.display = "none";
+    }
+}
+function mostrarOcultarDivPromo() {
+    var divPromo = document.getElementById("divPromo");
+
+    // Verifica si el div está visible
+    if (divPromo.style.display === "none" || divPromo.style.display === "") {
+        // Si está oculto, lo muestra
+        divPromo.style.display = "block";
+    } else {
+        // Si está visible, lo oculta
+        divPromo.style.display = "none";
+    }
+}
